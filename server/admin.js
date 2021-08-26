5fb37de2e6ed5e44b9632f47c5324471185ab4d0
samah-abujwaied <samah.abujweed@yahoo.com>
2021-08-26 22:09:31 +0300

    connecting between back - front

--- server/admin.js
+++ server/admin.js
@@ -1,23 +1,23 @@
-'use strict';
+// 'use strict';
 
-const client = require('socket.io-client');
-const host = "http://localhost:4000/discord";
+// const client = require('socket.io-client');
+// const host = "http://localhost:4000/discord";
 
-const socket = client.connect(host);
+// const socket = client.connect(host);
 
-// pulling msgs 
-socket.emit('get_all');//dpne
-
-
-
-socket.on('res-client', msg =>{
-    console.log("admin got this msg from client: ", msg)
-    socket.emit('received-client', msg)
-});//done
+// // pulling msgs 
+// socket.emit('get_all');//dpne
 
 
 
-// socket.emit('admin_msg', "Ok client we will discuss");
-const value = process.argv.splice(2)[0];
-socket.emit('admin_msg', value);
-console.log("value : ", value);
+// socket.on('res-client', msg =>{
+//     console.log("admin got this msg from client: ", msg)
+//     socket.emit('received-client', msg)
+// });//done
+
+
+
+// // socket.emit('admin_msg', "Ok client we will discuss");
+// const value = process.argv.splice(2)[0];
+// 
+// console.log("value : ", value);
--- web/src/App.js
+++ web/src/App.js
@@ -4,19 +4,31 @@ import io from "socket.io-client"
 import "./App.css"
 
 function App() {
-	const [ state, setState ] = useState({ message: "", name: "" })
+	const [ state, setState ] = useState({ignoreyourmsg:"",yourmsg:"",check:false,check2:false ,message: "", name: "" ,acceptmsg:"",ignoremsg:"",themsg:"",ignmsg:""})
 	const [ chat, setChat ] = useState([])
-
+	// console.log(state.acceptmsg);
+	// console.log(state.ignoremsg);
 	const socketRef = useRef()
 
 	useEffect(
+		
 		() => {
-			socketRef.current = io.connect("http://localhost:8080")
-			socketRef.current.emit('get_all-client');
-			socketRef.current.on("admin-data", ({ name, message }) => {
+			socketRef.current = io.connect("http://localhost:4456/discord")
+			socketRef.current.on("sendaccept",msg=>{
+				console.log('mssssssg====',msg.msgreq,msg.massage);
+				setState({ acceptmsg:msg.msgreq,yourmsg:msg.massage, check:true})
+			})
+			socketRef.current.on("sendignore",msg=>{
+				console.log(msg.msgreq);
+				setState({ ignoremsg:msg.msgreq,ignoreyourmsg:msg.massageig, check2:true})
+			})
+			socketRef.current.on("admin-data", ({ name, message ,id}) => {
 				setChat([ ...chat, { name, message } ])
+				socketRef.current.emit('get_all-client');
+				// socketRef.current.emit('received-client', id)
 			})
 			return () => socketRef.current.disconnect()
+			
 		},
 		[ chat ]
 	)
@@ -30,6 +42,8 @@ function App() {
 		socketRef.current.emit("client_msg", { name, message })
 		e.preventDefault()
 		setState({ message: "", name })
+		
+		
 	}
 
 	const renderChat = () => {
@@ -40,11 +54,16 @@ function App() {
 				</h3>
 			</div>
 		))
+	
+		
 	}
 
 	return (
+	
+
 		<div className="card">
 			<form onSubmit={onMessageSubmit}>
+		
 				<h1>Messenger</h1>
 				<div className="name-field">
 					<TextField name="name" onChange={(e) => onTextChange(e)} value={state.name} label="Name" />
@@ -64,9 +83,22 @@ function App() {
 			<div className="render-chat">
 				<h1>Chat Log</h1>
 				{renderChat()}
+
+				
 			</div>
+
+			<section>
+			
+			{state.check &&  <h1>{state.acceptmsg} -------- {state.yourmsg}</h1>}  
+			</section>
+			<section>
+			{state.check2 && <h2>{state.ignoremsg} -------- {state.ignoreyourmsg} </h2>}
+		
+	     	</section>
+		
 		</div>
 	)
+
 }
 
 export default App
